cd8bff7968d7ca769a08d20a53948479a2441abf
Agostina Pérez <aggostinaperez@gmail.com>
2022-10-24 12:50:01 -0300

    fixed library bugs

--- libreria.h
+++ libreria.h
@@ -1,19 +1,53 @@
-#ifndef _LIBRERIA
-#define _LIBRERIA
+#ifndef LIBRERIA_H
+#define LIBRERIA_H
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
 typedef int E;
 typedef char C;
 typedef float F;
 typedef void V;
 
 E menu(V);
-V total(V);
-V promProv(V);
-V promCiud(V);
-V ciudadCalida(V);
-V ciudadFria(V);
-V diaFrio(V);
-V diaCalido(V);
-V plantarPimientos(V);
 
-#include "libreria.c"
+class City{
+private:
+	struct city;
+public:
+	City(V);
+	V total(FILE *);
+	V promProv(V);
+	V promCiud(V);
+	V ciudadCalida(V);
+	V ciudadFria(V);
+	V diaFrio(V);
+	V diaCalido(V);
+	V plantarPimientos(V);
+	
+};
+
+struct timestamp{
+	E day;
+	E month;
+	E hh;
+	E mm;
+};
+
+struct measurement{
+	struct timestamp time;
+	F temp;
+	F hum;
+};
+
+
+struct city{
+	struct city *next;
+	struct measurement m;
+	E cityId;
+	C cityName[50];
+};
+
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,66 +1,57 @@
 #include <iostream>
 #include "libreria.h"
+#include "libreria.c"
 using namespace std;
-struct timestamp{
-	E day;
-	E month;
-	E hh;
-	E mm;
-};
-
-struct measurement{
-	struct timestamp time;
-	F temp;
-	F hum;
-};
-
-
-struct city{
-	struct city *next;
-	struct measurement m;
-	E cityId;
-	C cityName[50];//capaz quiero q sea string pero vemos
-};
-
-
-
 
 int main(int argc, char *argv[]) {
 	E op=0;
-
+	
+	City ciudad;
+	
 	FILE *fp;
 	fp=fopen("./data_set.txt","r");
 	if (fp==NULL){
 		cout<<"Imposible abrir el archivo";
 		exit(1);
 	}
-
+	fclose(fp);
+	
 	do{
 		op=menu();
 		switch(op){
 		case 1:
-			total();
+			ciudad.total(fp);
 			break;
 		case 2:
+			ciudad.promProv();
 			break;
 		case 3:
+			ciudad.promCiud();
 			break;
 		case 4:
+			ciudad.ciudadCalida();
 			break;
 		case 5:
+			ciudad.ciudadFria();
 			break;
 		case 6:
+			ciudad.diaFrio();
 			break;
 		case 7:
+			ciudad.diaCalido();
 			break;
 		case 8:
+			ciudad.plantarPimientos();
 			break;
-		case 0: break;
+		case 0: 
+			break;
 		default:
-			cout<<"Lo sentimos, esta opción no existe, inténtelo de nuevo"<<endl;
+			cout<<"Lo sentimos, esta opci?n no existe, int?ntelo de nuevo"<<endl;
 			break;
 		}
 	}while(op!=0);
 	return 0;
 }
+
+
 
